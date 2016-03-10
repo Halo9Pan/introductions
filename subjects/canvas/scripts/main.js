e768a6c3db758fff12b65c2e8581aa3f13333193
Halo9Pan <halo9pan@gmail.com>
2016-03-11 01:03:50 +0800

    Format the Canvas index.html

--- subjects/canvas/scripts/main.js
+++ subjects/canvas/scripts/main.js
@@ -3,88 +3,92 @@ var C = (function (w, d) {
 		privateVariable = 1;
 
 	var allTextArea = function() {
-        return d.querySelectorAll('.code>textarea')
+    return d.querySelectorAll('.code>textarea')
 	}
 
 	var allFrame = function() {
-        return d.querySelectorAll('.code>iframe')
+    return d.querySelectorAll('.code>iframe')
 	}
 
 	var allRootContainer = function() {
-        return d.querySelectorAll('.root')
+    return d.querySelectorAll('.root')
 	}
 
 	var allCodeContainer = function() {
-        return d.querySelectorAll('.code')
+    return d.querySelectorAll('.code')
 	}
 
 	var allDemoContainer = function() {
-        return d.querySelectorAll('.demo')
+    return d.querySelectorAll('.demo')
 	}
-    
-    var nodeListArray = function(nl) {
-        var arr = Array.prototype.slice.call(nl);
-        return arr;
-    }
-    
-    var loadPages = function() {
-        var rootContainerList = allRootContainer();
-        var rootContainers = nodeListArray(rootContainerList);
-        rootContainers.forEach(loadPage);
-    }
+  
+  var nodeListArray = function(nl) {
+    var arr = Array.prototype.slice.call(nl);
+    return arr;
+  }
+  
+  var loadPages = function() {
+    var rootContainerList = allRootContainer();
+    var rootContainers = nodeListArray(rootContainerList);
+    rootContainers.forEach(loadPage);
+  }
 
-    var loadPage = function(container, index, array) {
-        var id = container.id;
-        var href = "pages/default.html"
-        if(id) {
-            href = "pages/" + id + ".html";
-        }
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", href, false);
-        xmlhttp.send();
-        var content = xmlhttp.responseText;
-        var codeTextArea = container.querySelector('textarea');
-        codeTextArea.textContent = content;
+  var loadPage = function(container, index, array) {
+    var id = container.id;
+    var href = "pages/default.html"
+    if(id) {
+      href = "pages/" + id + ".html";
     }
-    
-    var initPreview = function() {
-        var nl = allTextArea();
-        var ata = nodeListArray(nl);
-        ata.forEach(preview);
-    }
-    
-    var preview = function(ta, index, array) {
-        var codeContainer = ta.parentNode;
-        var rootContainer = codeContainer.parentNode;
-        var demoContainer = rootContainer.querySelector('.demo');
-        var f = demoContainer.querySelector('iframe');
-        var delay;
-        var editor = CodeMirror.fromTextArea(ta, {
-            lineWrapping: true,
-            lineNumbers: true,
-            fixedGutter: false,
-            mode: 'text/html'
-        });
-        editor.on("change", function() {
-            clearTimeout(delay);
-            delay = setTimeout(function(){
-                updatePreview(f, editor);
-            }, 300);
-        });
-        setTimeout(function(){
-                updatePreview(f, editor);
-            }, 300);
-    }
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", href, false);
+    xmlhttp.send();
+    var content = xmlhttp.responseText;
+    var codeTextArea = container.querySelector('textarea');
+    codeTextArea.textContent = content;
+  }
+  
+  var initPreview = function() {
+    var nl = allTextArea();
+    var ata = nodeListArray(nl);
+    ata.forEach(preview);
+  }
+  
+  var preview = function(ta, index, array) {
+    var codeContainer = ta.parentNode;
+    var rootContainer = codeContainer.parentNode;
+    var demoContainer = rootContainer.querySelector('.demo');
+    var f = demoContainer.querySelector('iframe');
+    var delay;
+    var editor = CodeMirror.fromTextArea(ta, {
+      lineWrapping: true,
+      lineNumbers: true,
+      fixedGutter: false,
+      mode: 'text/html'
+    });
+    editor.on("mousedown", function() {
+      updatePreview(f, editor);
+    });
+    editor.on("change", function() {
+      clearTimeout(delay);
+      delay = setTimeout(function(){
+        updatePreview(f, editor);
+      }, 300);
+    });
+    setTimeout(function(){
+        updatePreview(f, editor);
+      }, 300);
+  }
 
-    var updatePreview = function(f, e) {
-        if(f && e){
-            var preview =  f.contentDocument ||  f.contentWindow.document;
-            preview.open();
-            preview.write(e.getValue());
-            preview.close();
-        }
+  var updatePreview = function(f, e) {
+    if(f && e){
+      e.refresh();
+      var preview =  f.contentDocument ||  f.contentWindow.document;
+      preview.open();
+      preview.write(e.getValue());
+      preview.close();
     }
-    
+  }
+  
 	r.moduleProperty = 1;
 	r.init = {preview: initPreview, content: loadPages};
 
